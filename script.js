9b451537c0ad827dc80e0ba24f422482fd703cdd
iceburg7 <ecburgess1@gmail.com>
2013-12-16 02:56:53 -0500

    making buttons

--- script.js
+++ script.js
@@ -55,24 +55,74 @@ $(document).ready(function(){
 	
 	//Add a wrapper with the background image
 	
-	$("body").append('<div class="wrapper"></div>');
 	
-	if(size.height < 650) {
-		$(".wrapper").css("backgroundImage", i("background500"));
-	}else if(size.height < 800) {
+	
+	
+	$("body").append('<div class="wrapper"><div class="wall"></div></div>');
+	
+//	if(size.height < 650) {
+//		$(".wrapper").css("backgroundImage", i("background500"));
+//	}else if(size.height < 800) {
 		$(".wrapper").css("backgroundImage", i("background700"));
-	}else{
-		$(".wrapper").css("backgroundImage", i("background"));
-	};
+//	}else{
+//		$(".wrapper").css("backgroundImage", i("background"));
+//	};
 	
 	$(".wrapper").css("backgroundRepeat", "no-repeat");
 	$(".wrapper").css("backgroundPosition", "center");
 	
-	$(".linklisting").appendTo("body");
+	
+	var postNumber = localStorage.getItem("post");
+	
+	if (postNumber > 0) {
+		var postNumber = localStorage.getItem("post");
+		console.log("pN was true");
+		console.log(postNumber);
+	}else{
+		localStorage.setItem("post", 1);
+		var postNumber = 1;
+		console.log("pN was false");
+		console.log(postNumber);
+	};
+	
+	
+	
+	$(".linklisting").appendTo(".wall");
 	$(".linklisting").children().addClass("hide");
-	$(".linklisting").children(":nth-child(7)").removeClass("hide");
+	$(".linklisting").children(":nth-child(" + postNumber + ")").removeClass("hide");
+	
 
 	
+	$(".wall").append('<div class="firstNav"></div><div class="previousNav"></div><div class="nextNav"></div>');
+	$(".firstNav").css("backgroundImage", i("first"));
+	$(".previousNav").css("backgroundImage", i("previous"));
+	$(".nextNav").css("backgroundImage", i("next"));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+//	var testget = $(".linklisting").get(0);
+//	console.log(testget);
+//	console.log( $("#siteTable")[3] );
+//	var testget2 = $("#siteTable").get(0);
+//	console.log(testget2);
+//	
+//	$(".linklisting").children(":nth-child(1)").addClass("1");
+//	console.log($(".linklisting").children(":nth-child(1)"));
+//	
+//	var testget = $(".1").get(0);
+	
+	
+	
+
+	$("wall").append('<div><iframe src="http://www.reddit.com/r/funny/" id="serverurl" width="300" height="300"></iframe></div>');
 	
 	
 	//	$(".linklisting").children().each(function(index) {
