9ef73851ca8e84040fc1e57a8463f3624fe1134d
iceburg7 <ecburgess1@gmail.com>
2013-12-15 22:46:13 -0500

    post on a wall

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+
 $(document).ready(function(){
 
 	
 	
+	
+	//Get browser size
+	
+	var size = {
+		width: window.innerWidth || document.body.clientWidth,
+		height: window.innerHeight || document.body.clientHeight
+	}
+	console.log(size);
 	
 	//Set things up to be able to use local images in the extension:
 	
@@ -32,15 +41,53 @@ $(document).ready(function(){
 	
 	
 	
+	//test for loading text and images
+	
+//	$("body").append("<p>test</p>");
+	
+//	$("body").append('<div class="test"></div>');
 	
 	
-	$(".content").append("<p>test</p>");
+//	var m = "testlol";
+//	$(".test").css("backgroundImage", i(m));
 	
-	$(".content").append('<div class="test"></div>');
 	
 	
-	var m = "testlol";
-	$(".test").css("backgroundImage", i(m));
+	//Add a wrapper with the background image
+	
+	$("body").append('<div class="wrapper"></div>');
+	
+	if(size.height < 650) {
+		$(".wrapper").css("backgroundImage", i("background500"));
+	}else if(size.height < 800) {
+		$(".wrapper").css("backgroundImage", i("background700"));
+	}else{
+		$(".wrapper").css("backgroundImage", i("background"));
+	};
+	
+	$(".wrapper").css("backgroundRepeat", "no-repeat");
+	$(".wrapper").css("backgroundPosition", "center");
+	
+	$(".linklisting").appendTo("body");
+	$(".linklisting").children().addClass("hide");
+	$(".linklisting").children(":nth-child(7)").removeClass("hide");
+
+	
+	
+	
+	//	$(".linklisting").children().each(function(index) {
+//		console.log(index, $(this));
+//	});
+	
+//	var posts = $(".linklisting")
+//	console.log(posts);
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
